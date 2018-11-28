96dd5bbc86783fe457e17c7c64f26538c500c405
3A532083 <luckyday604@gm.student.ncut.edu.tw>
2018-11-28 13:59:09 +0800

    建立common和task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+
+
+use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TaskController extends Controller
 {
-    //
+    /**
+     * 建立一個新的控制器實例。
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * 顯示使用者所有任務的清單。
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        return view('tasks.index');
+    }
 }
