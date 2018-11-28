f664dc3f52babd107a9fbfd2113644e01948ea9c
3A532083 <luckyday604@gm.student.ncut.edu.tw>
2018-11-28 14:04:10 +0800

    task資料庫

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/11/28
+ * Time: 下午 02:03
+ */
+namespace App\Repositories;
+use App\User;
+use App\Task;
+class TaskRepository
+{
+    /**
+     * 取得給定使用者的所有任務。
+     *
+     * @param  User  $user
+     * @return Collection
+     */
+    public function forUser(User $user)
+    {
+        return Task::where('user_id', $user->id)
+            ->orderBy('created_at', 'asc')
+            ->get();
+    }
+} 
